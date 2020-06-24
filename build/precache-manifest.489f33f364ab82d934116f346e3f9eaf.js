2dd70e248c39615e0210f8cbca4187772f62212a
Jake Dahl <jakedahl16@yahoo.com>
2020-06-24 17:44:37 -0500

    first commit

---
+++ build/precache-manifest.489f33f364ab82d934116f346e3f9eaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022c76cb6e37f58fa42af3695a01ba1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba95fd444a36fd0ab420",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "bca65b6a2a13a9060796",
+    "url": "/static/js/2.629e27ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.629e27ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba95fd444a36fd0ab420",
+    "url": "/static/js/main.bd24071a.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
